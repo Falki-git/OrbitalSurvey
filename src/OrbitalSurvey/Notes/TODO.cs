c07ec8fce0caf773af4ff280b0bbb8cac4ca2161
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-10-22 20:34:45 +0200

    Move note files to notes folder

---
+++ src/OrbitalSurvey/Notes/TODO.cs
@@ -0,0 +1,21 @@
+﻿
+/*
+ * 
+ * - determine texture pixels to reveal depending on vessel longitude and latitude
+ * - save/load persistance of mapping data
+ * - nonactive vessel mapping
+ * - scanning module on antennas
+ * - analytic gatheric of data while timewarping
+ * - minimum and maximum altitude for mapping
+ * - add OrbitalSurvey module to all antennas with Patch Manager
+ * - transmitting scans
+ * - buffer scans until transmitted
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
