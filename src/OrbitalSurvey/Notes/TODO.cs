cfcf411c6ffefb91955ac58b445f2860f3c149d2
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-11-11 11:01:57 +0100

    Update TODO.cs

--- src/OrbitalSurvey/Notes/TODO.cs
+++ src/OrbitalSurvey/Notes/TODO.cs
@@ -3,6 +3,7 @@
  * 
  * - determine texture pixels to reveal depending on vessel longitude and latitude
  * - save/load persistance of mapping data
+ * 
  * - nonactive vessel mapping
  * - scanning module on antennas
  * - analytic gatheric of data while timewarping
@@ -23,5 +24,14 @@
                 KSPAudioEventManager.onGameQuickSave();
                 KSPAudioEventManager.OnResourceTransfertStart();
             }
+ *
+ *
+ *
+ *
+ * OAB part description:
+ * - scanning cone
+ * - minimum altitude
+ * - maximum altitude
+ * - ideal altitude (do we need this?)
  * 
  */
