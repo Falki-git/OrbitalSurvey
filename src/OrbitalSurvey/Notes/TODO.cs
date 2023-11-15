467663c272b33ae0a6d385144711edc6af91594f
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-11-15 22:47:44 +0100

    Update TODO.cs

--- src/OrbitalSurvey/Notes/TODO.cs
+++ src/OrbitalSurvey/Notes/TODO.cs
@@ -3,12 +3,12 @@
  * 
  * - determine texture pixels to reveal depending on vessel longitude and latitude
  * - save/load persistance of mapping data
- * 
- * - nonactive vessel mapping
  * - scanning module on antennas
- * - analytic gatheric of data while timewarping
- * - minimum and maximum altitude for mapping
  * - add OrbitalSurvey module to all antennas with Patch Manager
+ * 
+ * - nonactive vessel mapping 
+ * - analytic gathering of data while timewarping
+ * - minimum and maximum altitude for mapping 
  * - transmitting scans
  * - buffer scans until transmitted
  * 
