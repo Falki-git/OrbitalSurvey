786e6a5dc53516449762da3a0358b51cdfd32d24
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-11-24 09:53:14 +0100

    Leave only 1k resolution for optimization

--- src/OrbitalSurvey/Utilities/Settings.cs
+++ src/OrbitalSurvey/Utilities/Settings.cs
@@ -4,6 +4,6 @@ public static class Settings
 {
     public const bool WILL_DEBUG_WINDOW_OPEN_ON_GAME_LOAD = true;
     public const double TIME_BETWEEN_SCANS = 1f;
-    public static readonly List<int> AVAILABLE_RESOLUTIONS = new() { 1024, 2048 };
+    public static readonly List<int> AVAILABLE_RESOLUTIONS = new() { 1024 };
     public static int ActiveResolution = 1024;
 }
