e369324d2ba1ff7a89da70c8a0b0a9a2956fa32e
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-11-27 15:01:01 +0100

    Fix retroactive scan not working correctly on some bodies

--- src/OrbitalSurvey/Utilities/OrbitUtility.cs
+++ src/OrbitalSurvey/Utilities/OrbitUtility.cs
@@ -12,20 +12,13 @@ public static class OrbitUtility
     
     public static void GetOrbitalParametersAtUT(VesselComponent vessel, double UT, out double latitude, out double longitude, out double altitude)
     {
-        Position position = new Position(vessel.Orbit.referenceBody.coordinateSystem, vessel.Orbit.WorldPositionAtUT(UT));
-        vessel.Orbit.referenceBody.GetLatLonAltFromRadius(position, out latitude, out longitude, out altitude);
+        var position = new Position(vessel.Orbit.ReferenceFrame, vessel.Orbit.GetRelativePositionAtUT(UT));
         
+        vessel.Orbit.referenceBody.GetLatLonAltFromRadius(position, out latitude, out longitude, out altitude);
         longitude += GetLongitudeOffsetDueToRotationForAGivenUT(vessel.Orbit.referenceBody, UT);
 
         if (longitude < -180f)
             longitude += 360f;
-
-        // directRotAngle = 254.234223135939
-        // rotationAngle = 254.234223135939
-        // rotationPeriod = 21549.425 (in seconds?)
-
-        // sphereOfInfluence = 84159286.4796305
-        // radius = 600000
     }
 
     public static double GetLongitudeOffsetDueToRotationForAGivenUT(CelestialBodyComponent body, double UT)
