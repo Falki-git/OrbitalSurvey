d27c03c3a0643ec6e0afd68015cf12495e9517de
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-12-04 19:48:35 +0100

    Close UI and remove overlay on every scene change (except Flight <-> Map)

--- src/OrbitalSurvey/Managers/MessageListener.cs
+++ src/OrbitalSurvey/Managers/MessageListener.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx.Logging;
 using KSP.Game;
 using KSP.Messages;
+using OrbitalSurvey.UI;
 using OrbitalSurvey.Utilities;
 
 namespace OrbitalSurvey.Managers;
@@ -10,7 +11,7 @@ public class MessageListener
     private ManualLogSource _logger = Logger.CreateLogSource("OrbitalSurvey.MessageListener");
     private static MessageListener _instance;
     public MessageCenter MessageCenter => GameManager.Instance.Game.Messages;
-    public GameStateConfiguration GameState => GameManager.Instance.Game.GlobalGameState.GetGameState();
+    //public GameStateConfiguration GameState => GameManager.Instance.Game.GlobalGameState.GetGameState();
 
     private MessageListener()
     { }
@@ -33,8 +34,10 @@ public class MessageListener
 
         MessageCenter.PersistentSubscribe<GameLoadFinishedMessage>(OnGameLoadFinishedMessage);
         _logger.LogInfo("Subscribed to GameLoadFinishedMessage.");
+        MessageCenter.PersistentSubscribe<GameStateChangedMessage>(OnGameStateChangedMessage);
+        _logger.LogInfo("Subscribed to GameStateChangedMessage.");
     }
-
+    
     private void OnGameLoadFinishedMessage(MessageCenterMessage message)
     {
         _logger.LogDebug("GameLoadFinishedMessage triggered.");
@@ -46,5 +49,28 @@ public class MessageListener
         }
         
         DEBUG_UI.Instance.IsDebugWindowOpen = Settings.WILL_DEBUG_WINDOW_OPEN_ON_GAME_LOAD;
+    }
+    
+    private void OnGameStateChangedMessage(MessageCenterMessage obj)
+    {
+        var msg = obj as GameStateChangedMessage;
+        
+        // Close GUI and remove overlay on every scene change except Flight <-> Map
+        if (msg.PreviousState == GameState.FlightView && 
+            (msg.CurrentState == GameState.Map3DView || msg.CurrentState == GameState.FlightView))
+        {
+            return;
+        }
+        
+        if (msg.PreviousState == GameState.Map3DView && 
+            (msg.CurrentState == GameState.Map3DView || msg.CurrentState == GameState.FlightView))
+        {
+            return;
+        }
+
+        if(SceneController.Instance.ShowMainGui)
+            SceneController.Instance.ShowMainGui = false;
+        
+        OverlayManager.Instance.RemoveOverlay();
     }
 }
