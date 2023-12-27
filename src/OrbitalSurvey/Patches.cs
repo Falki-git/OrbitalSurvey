abb972959d3ab04474ce46609e6c0c26b190450f
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-12-28 00:13:41 +0100

    Fix log spam "experiment moved to a valid location, but something else is preventing the experiment from being ready."

--- src/OrbitalSurvey/Patches.cs
+++ src/OrbitalSurvey/Patches.cs
@@ -1,7 +1,9 @@
 ﻿using HarmonyLib;
 using I2.Loc;
-using KSP.Messages;
+using KSP.Game.Science;
+using KSP.Logging;
 using KSP.Modules;
+using KSP.Sim.impl;
 
 namespace OrbitalSurvey;
 
@@ -24,7 +26,7 @@ public class Patches
 
         return true;
     }
-    /*
+    
     /// <summary>
     /// Also used to remove experiments from PAM
     /// </summary>
@@ -41,7 +43,7 @@ public class Patches
 
         return true;
     }
-    */
+    
     /// <summary>
     /// This removes experiment descriptions on parts in OAB
     /// Since there are experiments for 25%, 50%, 75% and 100% and then for High and Low orbits, there would be too much
@@ -84,17 +86,57 @@ public class Patches
         }
     }
     
-    [HarmonyPatch(typeof(Module_ScienceExperiment), "OnScienceSituationChanged"), HarmonyPrefix]
-    private static bool Test(MessageCenterMessage msg, Module_ScienceExperiment __instance)
+    /// <summary>
+    /// Reimplementation of the original method with the following log spam being omitted:
+    /// "[Unity] [Simulation] Science Experiment {experiment name} moved to a valid location, but something else is preventing the experiment from being ready." 
+    /// </summary>
+    [HarmonyPatch(typeof(PartComponentModule_ScienceExperiment), "RefreshLocationsValidity"), HarmonyPrefix]
+    private static bool RefreshLocationsValidity(PartComponentModule_ScienceExperiment __instance)
     {
-        return false;
-        
-        VesselScienceSituationChangedMessage vesselScienceSituationChangedMessage = msg as VesselScienceSituationChangedMessage;
-        if (vesselScienceSituationChangedMessage != null && __instance._vesselComponent != null && vesselScienceSituationChangedMessage.Vessel.GlobalId.Equals(__instance._vesselComponent.GlobalId))
-        {
-            __instance.UpdatePAM();
-        }
+        if (__instance._vesselComponent == null || __instance._vesselComponent.mainBody == null || __instance._vesselComponent.VesselScienceRegionSituation.ResearchLocation == null)
+		{
+			return false;
+		}
+		__instance._currentLocation = new ResearchLocation(true, __instance._vesselComponent.mainBody.bodyName, __instance._vesselComponent.VesselScienceRegionSituation.ResearchLocation.ScienceSituation, __instance._vesselComponent.VesselScienceRegionSituation.ResearchLocation.ScienceRegion);
+		int count = __instance.dataScienceExperiment.ExperimentStandings.Count;
+		while (count-- > 0)
+		{
+			ExperimentDefinition experimentDefinition = __instance.dataScienceExperiment.ExperimentStandings[count].ExperimentDefinition;
+			__instance.dataScienceExperiment.ExperimentStandings[count].CurrentSituationIsValid = experimentDefinition.IsLocationValid(__instance._currentLocation, out __instance.dataScienceExperiment.ExperimentStandings[count].RegionRequired) && __instance._vesselComponent.VesselScienceRegionSituation.SituationScalar > 0f && __instance._vesselComponent.VesselScienceRegionSituation.ScienceRegionScalar > 0f;
+			__instance._currentLocation.RequiresRegion = (__instance.dataScienceExperiment.ExperimentStandings[count].CurrentSituationIsValid ? __instance.dataScienceExperiment.ExperimentStandings[count].RegionRequired : __instance._currentLocation.RequiresRegion);
+			if (__instance.dataScienceExperiment.ExperimentStandings[count].CurrentSituationIsValid)
+			{
+				if (__instance.dataScienceExperiment.ExperimentStandings[count].CurrentExperimentState == ExperimentState.RUNNING)
+				{
+					__instance.StopExperiment(__instance.dataScienceExperiment.Experiments[count].ExperimentDefinitionID, true, false);
+				}
+				else if (__instance.dataScienceExperiment.ExperimentStandings[count].CurrentExperimentState != ExperimentState.RUNNING && __instance.dataScienceExperiment.ExperimentStandings[count].CurrentExperimentState != ExperimentState.PAUSED)
+				{
+					__instance.SetExperimentState(count, ExperimentState.READY);
+				}
+			}
+			else if (__instance.dataScienceExperiment.ExperimentStandings[count].CurrentExperimentState == ExperimentState.RUNNING)
+			{
+				__instance.dataScienceExperiment.ExperimentStandings[count].CurrentExperimentContext = ScienceRegionsHelper.GetRegionDisplayName(__instance.dataScienceExperiment.LastKnownValidSituation.ResearchLocation.ScienceRegion);
+				__instance.StopExperiment(__instance.dataScienceExperiment.ExperimentStandings[count].ExperimentID, false, false);
+				__instance.TrySendStateChangeMessage(count, true);
+			}
+			else
+			{
+				__instance.dataScienceExperiment.ExperimentStandings[count].CurrentExperimentState = ExperimentState.INVALIDLOCATION;
+			}
+			switch (__instance.dataScienceExperiment.ExperimentStandings[count].CurrentExperimentState)
+			{
+			case ExperimentState.NONE:
+			case ExperimentState.LOCATIONCHANGED:
+				__instance.SetExperimentState(count, ExperimentState.READY);
+				continue;
+			case ExperimentState.INVALIDLOCATION:
+				GlobalLog.LogF(LogFilter.Simulation, "Science Experiment {0} moved to an invalid location, pausing.", new object[] { experimentDefinition.DisplayName });
+				continue;
+			}
+		}
 
-        return false;
+		return false;
     }
 }
